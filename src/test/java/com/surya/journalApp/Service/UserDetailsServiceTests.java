554b511a314f8a7a1b3d04ed0bd5e196bd42ac46
Suryakumarkv <suryakumarkv30@gmail.com>
2026-04-16 22:46:11 +0530

    first commit

--- src/test/java/com/surya/journalApp/Service/UserDetailsServiceTests.java
+++ src/test/java/com/surya/journalApp/Service/UserDetailsServiceTests.java
@@ -2,22 +2,16 @@ package com.surya.journalApp.Service;
 
 import com.surya.journalApp.Entity.User;
 import com.surya.journalApp.Repository.UserRepository;
-import org.assertj.core.util.Strings;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.CsvSources;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.mongodb.internal.connection.tlschannel.util.Util.assertTrue;
 
 @SpringBootTest
-public class UserDetailsServiceTests {
+ class UserDetailsServiceTests {
 
     @Autowired
     private UserRepository userRepository;
@@ -25,33 +19,14 @@ public class UserDetailsServiceTests {
     @Autowired
     private UserService userService;
 
+    @BeforeEach
+    void setup() {
+        userRepository.deleteAll();   // 🔥 IMPORTANT
+    }
+
     @ParameterizedTest
     @ArgumentsSource(UserArgumentsProvider.class)
-    public void testSaveUserDetails(User user) {
-         assertTrue(userService.saveNewUser(user));
+     void testSaveUserDetails(User user) {
+        assertTrue(userService.saveNewUser(user));
     }
-
-    @Disabled
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "poorva",
-            "poornima",
-            "suryaKumarK"
-    })
-    public void testFindByUsername(String name) {
-        assertNotNull(userRepository.findByUserName(name));
-        assertEquals(4, 2 + 2);
-    }
-
-    @Disabled
-    @ParameterizedTest
-    @CsvSource({
-            "2,2,4",
-            "4,7,11",
-            "1,2,4"
-    })
-    public void test(int a, int b, int expected) {
-        assertEquals(expected, a + b);
-    }
-
 }
