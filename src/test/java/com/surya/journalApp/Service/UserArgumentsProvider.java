94d37a06e03971f171d870d3caa616c74e4ee665
Suryakumarkv <suryakumarkv30@gmail.com>
2026-04-17 03:14:33 +0530

    fix java version issue

--- src/test/java/com/surya/journalApp/Service/UserArgumentsProvider.java
+++ src/test/java/com/surya/journalApp/Service/UserArgumentsProvider.java
@@ -3,7 +3,6 @@ package com.surya.journalApp.Service;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.support.ParameterDeclarations;
 import com.surya.journalApp.Entity.User;
 
 
